370bcff8e08d74dd04366a144fb10cedfa1fc33a
Dmitry Diordychuk <cort@mail.ru>
2021-11-05 17:47:47 +0300

    rm

--- django-gallery/gallery/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Image(models.Model):
-	title = models.CharField(null=False, max_length=50)
-	image = models.FileField(upload_to='images')
--- django-gallery/gallery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import GalleryView
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-	path('', GalleryView.as_view(), name="gallery"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
